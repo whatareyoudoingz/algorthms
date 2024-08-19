6e64e2a3c7666b91e71a4ac81bfce60e756a1229
Jin Ah Kim <wlsdk3165@naver.com>
2024-08-19 23:06:18 +0900

    Update 벌집.py

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,5 +1,5 @@
 x=int(input())
-z,i,l=0,0,0
+z,i,l=0,0,0 # 범위의 끝값, i : 범위의 끝값을 구하기 위해 필요한 규칙 수, l : 결과값 
 while z<=x:
     l+=1
     if x in range(z,6*i+2):
@@ -8,3 +8,26 @@ while z<=x:
     else:
         z=6*i+2
         i+=l
+
+# 수학적 규칙
+# range(0,2)
+# range(2,8)
+# range(8,20)
+# range(20,38)
+# range(38,62)
+
+# 2 - 8 - 20 - 38 - 62
+# 6*0+2 - 6*(0+1)+2 - 6*(0+1+2)+2 - ... 
+# f(i)=6*i+2
+
+
+## 다른 사람 풀이
+n = int(input())
+nbox = 1
+cnt = 1
+while n > nbox:
+    nbox += 6 * cnt
+    cnt += 1
+print(cnt)
+
+# 차이점 : 나보다 변수를 1개 줄여 변수 2개로 줄였고, if문을 사용하지 않아 코드가 간소화되었다. 
