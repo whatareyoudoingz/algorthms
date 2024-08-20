2cedf130fc020b3452c55cdf5603c5d2206ee774
Jin Ah Kim <wlsdk3165@naver.com>
2024-08-20 20:58:59 +0900

    Update 달팽이는 올라가고 싶다.py

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,10 +1,19 @@
+#### 내 풀이 ####
 a,b,v=map(int,input().split())
 
-target=v-a 
-n,stan=target//(a-b) ,target%(a-b)
-if stan==0: 
+n,stan=(v-a)//(a-b) ,(v-a)%(a-b)
+if stan==0: # 나머지가 0이면 n+1 출력
     print(n+1)
-elif stan>=1: 
+elif stan>=1: # 나머지가 1이상이면 a와 합쳐서 n+2 출력
     print(n+2)
 else:
     print(n)
+
+#### 다른사람 풀이 ####
+import math
+a,b,v = map(int,input().split())
+day = (v-b)/(a-b)
+print(math.ceil(day))
+
+# 차이점 : 나는 a에 집중해서 수학식을 생각했다면, 다른 사람은 b에 집중해서 수학식을 작성했다.
+# 배울점 : 이렇게 간단하게 구현할 수 있다니.. 코테에서 사용할 수 있는 기본적인 모듈을 잘 숙지해야 겠다.
